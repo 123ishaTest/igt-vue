f0c26e43b8ef6fd9a2813090d519c34dd3e3ca8f
Isha Dijcks <ishadijcks@gmail.com>
2020-10-14 01:58:55 +0200

    Add tests for custom requirements

---
+++ tests/unit/ig-template/tools/requirements/MultiRequirement.spec.ts
@@ -0,0 +1,42 @@
+import {CustomRequirement} from "@/ig-template/tools/requirements/CustomRequirement";
+import {MultiRequirement} from "@/ig-template/tools/requirements/MultiRequirement";
+
+
+describe('Multi Requirement', () => {
+
+    test('one true one false', () => {
+        // Arrange
+        const multi = new MultiRequirement([
+            new CustomRequirement("Always true", () => 1, () => 1),
+            new CustomRequirement("Always false", () => 0, () => 1)
+        ]);
+
+        // Act
+        const isCompleted = multi.isCompleted;
+        const progress = multi.progress;
+
+        // Assert
+        expect(isCompleted).toBeFalsy();
+        expect(progress.actual).toBe(1);
+        expect(progress.target).toBe(2);
+    });
+
+    test('both true', () => {
+        // Arrange
+        const multi = new MultiRequirement([
+            new CustomRequirement("Always true", () => 1, () => 1),
+            new CustomRequirement("Always true", () => 1, () => 1),
+        ]);
+
+        // Act
+        const isCompleted = multi.isCompleted;
+        const progress = multi.progress;
+
+        // Assert
+        expect(isCompleted).toBeTruthy();
+        expect(progress.actual).toBe(2);
+        expect(progress.target).toBe(2);
+    });
+
+
+});
