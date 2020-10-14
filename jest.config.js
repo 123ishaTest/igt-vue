687a6e287f7b36a9801280e52aa3ca0a4eb221f2
Isha Dijcks <ishadijcks@gmail.com>
2020-10-14 15:19:54 +0200

    Fix formatting

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts}'],
-  testMatch: ["<rootDir>/tests/**/*.{ts, js}"]
-
+    preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts}'],
+    testMatch: ["<rootDir>/tests/**/*.{ts, js}"],
 }
