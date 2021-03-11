4ea1794c651241953e25ce1db268f02798933402
Isha <ishadijcks@gmail.com>
2021-03-11 15:22:45 +0100

    Improve statistics test

--- tests/unit/ig-template/features/statistics/Statistics.ts
+++ tests/unit/ig-template/features/statistics/Statistics.ts
@@ -1,13 +1,17 @@
 import {NumberStatistic} from "@/ig-template/features/statistics/NumberStatistic";
 import {StatisticId} from "@/ig-template/features/statistics/StatisticId";
 import {Statistics} from "@/ig-template/features/statistics/Statistics";
+import {ArrayStatistic} from "@/ig-template/features/statistics/ArrayStatistic";
 
 
 describe('Number Statistic', () => {
     const statistics = new Statistics();
     const id = 'example' as StatisticId;
+    const arrayId = 'array' as StatisticId;
     const stat = new NumberStatistic(id, 'get money', 0);
+    const array = new ArrayStatistic(arrayId, 'array stat', [0, 0, 0]);
     statistics.registerStatistic(stat);
+    const arrayStat = statistics.registerStatistic(array);
 
     beforeEach(() => {
         stat.value = 0;
@@ -25,6 +29,13 @@ describe('Number Statistic', () => {
     test('incorrect get', () => {
         // Assert
         expect(statistics.getStatistic('wrong-id' as StatisticId)).toBeNull();
+    });
+
+    test('array statistic', () => {
+        // Assert
+        statistics.incrementArrayStatistic(arrayStat.id, 1, 3)
+        expect(arrayStat.value[1]).toBe(3);
+
     });
 
     test('save and load', () => {
