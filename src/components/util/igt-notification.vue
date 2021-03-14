200596bc0ef7f7f35ce88a9e787e5ae8a48db8fa
Isha <ishadijcks@gmail.com>
2021-03-14 13:59:39 +0100

    Refactor notifications to support multiple types

---
+++ src/components/util/igt-notification.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex">
+    <div class="flex justify-center items-center w-12" :class="backgroundColor">
+      <div class="text-xl fa" :class="icon"></div>
+    </div>
+
+    <div class="-mx-3 py-2 px-4">
+      <div class="mx-3">
+        <span class="font-semibold" :class="textColor">{{ notification.title }}</span>
+        <p class="text-gray-600 text-sm">{{ notification.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "igt-notification",
+  props: {
+    notification: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    textColor() {
+      switch (this.notification.type) {
+        case 'success':
+          return 'text-green-500'
+        case 'error':
+          return 'text-red-500'
+        default:
+          return 'text-gray-500';
+      }
+    },
+    backgroundColor() {
+      switch (this.notification.type) {
+        case 'success':
+          return 'bg-green-500'
+        case 'error':
+          return 'bg-red-500'
+        default:
+          return 'bg-gray-500';
+      }
+    },
+    icon() {
+      switch (this.notification.type) {
+        case 'success':
+          return 'fa-check'
+        case 'error':
+          return 'fa-times'
+        default:
+          return 'fa-question';
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/util/igt-notifications.vue
+++ src/components/util/igt-notifications.vue
@@ -6,28 +6,11 @@
       <div class="max-w-sm w-full">
         <notification v-slot="{notifications}">
           <div
-              class="flex max-w-sm w-full mx-auto bg-white shadow-md rounded-lg overflow-hidden mt-4"
+              class="max-w-sm w-full mx-auto bg-white shadow-md rounded-lg overflow-hidden mt-4"
               v-for="notification in notifications"
               :key="notification.id"
           >
-            <div class="flex justify-center items-center w-12 bg-green-500">
-              <svg
-                  class="h-6 w-6 fill-current text-white"
-                  viewBox="0 0 40 40"
-                  xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                    d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z"
-                />
-              </svg>
-            </div>
-
-            <div class="-mx-3 py-2 px-4">
-              <div class="mx-3">
-                <span class="text-green-500 font-semibold">{{ notification.title }}</span>
-                <p class="text-gray-600 text-sm">{{ notification.text }}</p>
-              </div>
-            </div>
+            <igt-notification :notification="notification"></igt-notification>
           </div>
         </notification>
       </div>
@@ -37,8 +20,11 @@
 
 <script>
 
+import IgtNotification from "@/components/util/igt-notification";
+
 export default {
   name: "igt-notifications",
+  components: {IgtNotification},
 }
 </script>
 
