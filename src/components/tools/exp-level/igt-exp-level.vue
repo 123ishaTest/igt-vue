290089b68b17deaf4a91894999c7118aa87d9f0c
Isha <ishadijcks@gmail.com>
2021-03-15 23:02:37 +0100

    Refactor progress bar to own component

--- src/components/tools/exp-level/igt-exp-level.vue
+++ src/components/tools/exp-level/igt-exp-level.vue
@@ -3,14 +3,11 @@
 
     <div class="relative pt-1">
       <div class="flex justify-between">
-      <p>Lvl. {{ expLevel.getLevel() }} / {{ expLevel.maxLevel }}</p>
-      <p>{{ progress.actual | numberFormat }} / {{ progress.target  | numberFormat }}</p>
+        <p>Lvl. {{ expLevel.getLevel() }} / {{ expLevel.maxLevel }}</p>
+        <p>{{ progress.actual | numberFormat }} / {{ progress.target  | numberFormat }}</p>
 
       </div>
-      <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-pink-200">
-        <div :style="{'width' : progressPercentage * 100 + '%'}"
-             class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
-      </div>
+      <igt-progress-bar :percentage="progressPercentage"></igt-progress-bar>
 
     </div>
   </div>
@@ -20,9 +17,11 @@
 
 
 import {AbstractExpLevel} from "@/ig-template/tools/exp-level/AbstractExpLevel";
+import IgtProgressBar from "@/components/util/igt-progress-bar";
 
 export default {
   name: "igt-exp-level",
+  components: {IgtProgressBar},
   props: {
     expLevel: {
       type: AbstractExpLevel,
