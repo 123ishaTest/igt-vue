a8ac362e9bab7129f6cb53c8f0a849742cd5ac12
Isha <ishadijcks@gmail.com>
2021-03-18 20:55:59 +0100

    Fix achievements on mobile

--- src/components/features/achievements/igt-achievements.vue
+++ src/components/features/achievements/igt-achievements.vue
@@ -1,8 +1,8 @@
 <template>
   <igt-feature>
-    <div class="flex flex-row flex-wrap">
-      <div :key=achievement.id v-for="achievement in achievements" class="w-72">
-        <div class="flex flex-col border-2 m-2 p-2 h-72 justify-between shadow-md"
+    <div class="flex flex-row flex-wrap justify-center sm:justify-start">
+      <div :key=achievement.id v-for="achievement in achievements" class="w-64 sm:w-72">
+        <div class="flex flex-col border-2 m-2 p-2 h-64 sm:h-72 justify-between shadow-md"
              :class="{'bg-gray-400': !achievement.unlocked, 'bg-green-500': achievement.unlocked}">
           <div>
             <p class="text-lg font-semibold"> {{ achievement.unlocked ? achievement.title : '???' }}</p>
@@ -11,7 +11,7 @@
             <p v-if="achievement.isHidden && !achievement.unlocked" class="italic"> Hidden achievement </p>
             <p v-else class="italic"> {{ achievement.description }}</p>
           </div>
-          <img v-if="achievement.image" class="w-28 h-28 mx-auto m-2;"
+          <img v-if="achievement.image" class="w-20 h-20 sm:w-28 sm:h-28 mx-auto m-2;"
                :class="{'filter-grayscale': !achievement.unlocked}"
                :src="require(`@/assets/${achievement.image}`)" :alt="achievement.image">
         </div>
