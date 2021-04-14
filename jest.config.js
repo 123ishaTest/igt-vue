c77f6762c5aa7c741e72aaca0088c6342db3984b
Isha <ishadijcks@gmail.com>
2021-04-15 00:53:00 +0200

    Transform ignore lib

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     collectCoverageFrom: ['src/ig-template/**/*.{js,jsx,ts}'],
     testMatch: ["<rootDir>/tests/**/*.{ts, js}"],
     transformIgnorePatterns: [
-        "<rootDir>/node_modules/(?!lodash-es)"
+        "<rootDir>node_modules/(?!(lodash-es"
+        + "|incremental-game-template"
+        + ")/)",
     ]
 }
