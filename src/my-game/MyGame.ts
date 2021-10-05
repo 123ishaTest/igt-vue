30a009111c5e3700f22f95400c37bf0e2605813c
Isha <ishadijcks@gmail.com>
2021-10-06 01:16:46 +0200

    Revert "Update SAVE_KEY and TICK_DURATION"
    This reverts commit e1d244d77439ca22612bd88d97d963342acc7248.

--- src/my-game/MyGame.ts
+++ src/my-game/MyGame.ts
@@ -2,8 +2,10 @@ import {IgtGame} from "incremental-game-template";
 import {MyFeatures} from "@/my-game/MyFeatures";
 
 export class MyGame extends IgtGame {
-    protected readonly SAVE_KEY: string = '123ishaTest/igt-demo';
-    protected readonly TICK_DURATION: number = 0.1;
+    // @TODO Update SAVE_KEY to something unique
+    protected readonly SAVE_KEY: string = 'igt-vue';
+    // @TODO Update TICK_DURATION to an appropriate value
+    protected readonly TICK_DURATION: number = 0.01;
     features: MyFeatures;
 
     constructor(features: MyFeatures) {
