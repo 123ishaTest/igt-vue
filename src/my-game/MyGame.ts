e1d244d77439ca22612bd88d97d963342acc7248
Isha <ishadijcks@gmail.com>
2021-10-05 23:58:49 +0200

    Update SAVE_KEY and TICK_DURATION

--- src/my-game/MyGame.ts
+++ src/my-game/MyGame.ts
@@ -2,10 +2,8 @@ import {IgtGame} from "incremental-game-template";
 import {MyFeatures} from "@/my-game/MyFeatures";
 
 export class MyGame extends IgtGame {
-    // @TODO Update SAVE_KEY to something unique
-    protected readonly SAVE_KEY: string = 'igt-vue';
-    // @TODO Update TICK_DURATION to an appropriate value
-    protected readonly TICK_DURATION: number = 0.01;
+    protected readonly SAVE_KEY: string = '123ishaTest/igt-demo';
+    protected readonly TICK_DURATION: number = 0.1;
     features: MyFeatures;
 
     constructor(features: MyFeatures) {
